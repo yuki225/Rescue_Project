51c2be13edd21908bfc657fafd91aeb5ca47efa5
yuki225 <yuki.bay25@gmail.com>
2018-01-06 18:47:14 +0900

    test

--- auto.sh
+++ auto.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-for i in `seq 1 10`
-do
+#for i in `seq 1 10`
+#do
   cd ~/Rescue_Project
   python record.py
   cp human_voice.wav ~/sample_voice
   cp human_voice_filtered.wav ~/sample_voice
   cd ~/sample_voice
   julius -C ~/julius-kits/dictation-kit-v4.4/word.jconf -outfile -input rawfile
-  cd ~/Navio2/Python
-  python modGPS.py
-  cat ~/Navio2/Python/src/test.csv ~/Software/julius/test_wav/sample.out > ~/Rescue_Project/victim_data.csv
-done
+#  cd ~/Navio2/Python
+#  python modGPS.py
+#  cat ~/Navio2/Python/src/test.csv ~/Software/julius/test_wav/sample.out > ~/Rescue_Project/victim_data.csv
+#done
